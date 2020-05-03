06677a9f5afd035ccddbb685e020a6d7e216d5a1
Oscar Antonio Hernández Mojica <oscar2mojica@gmail.com>
2020-05-02 19:17:41 -0500

    reading fron a json file

--- main.py
+++ main.py
@@ -1,59 +1,37 @@
+import json
+import time
+import os
+
+from subprocess import check_call, check_output
+
 from auida import AndroidDevice
 from log_record import Log
-import time
 
+VERSION = 2.0
+
+DATA_TEST_FILE = 'data-test.json'
 DELAY = 8
 
-def is_valid_number(number):
-    """
-    Verify the number given if it is correct for realise a call
-    :param number: string Number to call given by the user
-    :return: boolean True if is valid number or False otherwise
-    """
-    if number[0] == '+' or number[0] == '*':
-        return number[1:].isdigit()
-    return number.isdigit()
-
 if __name__ == '__main__':
-    android = AndroidDevice()
-    android.select_device()
-    log = Log()
-    while True:
-        print('Call a number\n' +
-              '\t 1) by adb shell\n' +
-              '\t 2) by uiautomator\n' +
-              'Turn ON Wifi\n' +
-              '\t 3) by adb shell\n' +
-              '\t 4) by uiautomator\n' +
-              'Turn OFF Wifi\n' +
-              '\t 5) by adb shell\n' +
-              '\t 6) by uiautomator\n' +
-              '7) Exit')
-        option = input('Select an operation: ')
-        if option.isdigit():
-            option = int(option)
-            if option == 1:
-                number = input('Enter the number to call: ')
-                if is_valid_number(number):
-                    android.adb_calling_test(number, DELAY)
-            if option == 2:
-                number = input('Enter the number to call: ')
-                if is_valid_number(number):
-                    android.uia_calling_test(number, DELAY)
-            if option == 3:
-                android.adb_wifi_test('ON')
-            if option == 4:
-                # android.settings_wifi_test('ON')
-                android.uia_quick_wifi_test('ON')
-            if option == 5:
-                android.adb_wifi_test('OFF')
-            if option == 6:
-                # android.settings_wifi_test('OFF')
-                android.uia_quick_wifi_test('OFF')
-            if option == 7:
-                break
-            if option < 0 or option > 7:
-                print('{0} is not a valid number'.format(option))
-        else:
-            print('{0} is not a valid number'.format(option))
+    test_path = os.path.join(os.getcwd(), DATA_TEST_FILE)
+    if not os.path.exists(test_path):
+        print("Test data file not found")
+    else:
+        android = AndroidDevice()
+        android.select_device()
+        print(android.device)
+        check_call(['adb', '-s', android.device, 'shell', 'am', 'start',
+                        '-a', 'android.intent.action.CALL', '-d', 'tel:{0}'.format('911')])
+        # log = Log()
+        # android.dial_number('911')
+        time.sleep(3)
+        # with open('data-test.json') as json_file:
+        #     data = json.load(json_file)
+        #     for phone in data['phones']:
+        #         # android.adb_calling_test(phone, DELAY)
+        #         time.sleep(10)
 
+        #     android.initial_state()
+
+            # for operation in data['operations']:
+            #     print(operation[0])
